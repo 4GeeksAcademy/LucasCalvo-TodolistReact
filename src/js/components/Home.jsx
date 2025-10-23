a21ba37ea40bde4d4b086e05d79f8e27b98dac1c
Lucas Matias Calvo <lucascalvo2903@gmail.com>
2025-10-23 02:04:57 +0000

    proyecto finalizado

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -18,14 +18,15 @@ const eliminarTarea = (posicion) =>{
 }
 
 	return (
-	<div className="text-center d-flex flex-column">	
-	    <h1 className="text-secondary">TodoList</h1>
+	<div className="d-flex flex-column">	
+	    <h1 className="text-secondary text-center">TodoList</h1>
 		<input className="form-control p-4" type="text"  placeholder="agrega una tarea" onChange={escribirTarea} value={tarea} onKeyDown={agregar}/>
-        <ul className="list-unstyled text-start">
-			{lista.map((item,index)=>(<li key={index} className="paper">
+        <ul className="list-unstyled text-start list-group">
+			{lista.map((item,index)=>(<li className="list-group-item" key={index}>
 			{item}<button onClick={()=>eliminarTarea(index)}>❌</button></li>))}
 		</ul>
-        <p className="me-auto">Tienes {lista.length} {lista.length === 1 ? "tarea" : "tareas"}</p>
+			<div className="paper" >Tienes {lista.length} {lista.length === 1 ? "tarea" : "tareas"}</div>
+		
   </div>
   
 			
