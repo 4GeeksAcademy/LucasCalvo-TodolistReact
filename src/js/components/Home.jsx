e4ccaf6f3ad933313c3568184cbd8b8f1b82db84
Lucas Matias Calvo <lucascalvo2903@gmail.com>
2025-10-28 00:45:56 +0000

    modificacion del boton

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,37 +1,41 @@
 import React,{useState} from "react";
 
-
 const Home = () => {
 	let [tarea, setTarea]=useState("")
 	let [lista, setLista]=useState([])
+
     const escribirTarea = (event) =>{
 		setTarea (event.target.value)
     }
+
 	const agregar = (event) =>{
 		if (event.key==="Enter") {
-		setLista ([...lista, tarea])
-		setTarea ("")
-	}
-}
-const eliminarTarea = (posicion) =>{
-	setLista (lista.filter ((item, index)=>index!==posicion))
-}
+		    setLista ([...lista, tarea])
+		    setTarea ("")
+	    }
+    }
+
+	const eliminarTarea = (posicion) =>{
+	    setLista (lista.filter ((item, index)=>index!==posicion))
+    }
 
 	return (
 	<div className="d-flex flex-column">	
 	    <h1 className="text-secondary text-center">TodoList</h1>
-		<input className="form-control p-4" type="text"  placeholder="agrega una tarea" onChange={escribirTarea} value={tarea} onKeyDown={agregar}/>
+		<input className="form-control p-4" type="text" placeholder="agrega una tarea" onChange={escribirTarea} value={tarea} onKeyDown={agregar}/>
+        
         <ul className="list-unstyled text-start list-group">
-			{lista.map((item,index)=>(<li className="list-group-item" key={index}>
-			{item}<button onClick={()=>eliminarTarea(index)}>❌</button></li>))}
+			{lista.map((item,index)=>(
+				<li className="list-group-item d-flex justify-content-between align-items-center" key={index}>
+				    <span>{item}</span>
+				    <button onClick={()=>eliminarTarea(index)}>❌</button>
+				</li>
+			))}
 		</ul>
-			<div className="paper" >Tienes {lista.length} {lista.length === 1 ? "tarea" : "tareas"}</div>
-		
-  </div>
-  
-			
+
+		<div className="paper">Tienes {lista.length} {lista.length === 1 ? "tarea" : "tareas"}</div>
+	</div>
 	);
 };
 
 export default Home;
-
